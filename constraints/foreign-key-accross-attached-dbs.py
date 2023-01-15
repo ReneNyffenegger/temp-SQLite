71a074fc4e9a27ff87fafdc63281ae233b4818b9
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-01-15 18:44:27 +0100

    .

--- constraints/foreign-key-accross-attached-dbs.py
+++ constraints/foreign-key-accross-attached-dbs.py
@@ -3,11 +3,15 @@ import os.path
 # import sys
 # import re
 
-dbName = 'foreign-key-accross-attached-dbs.db'
-if os.path.isfile(dbName):
-   os.remove(dbName)
+dbName1 = 'foreign-key-accross-attached-dbs.db'
+dbName2 = 'x.db'
 
-db = sqlite3.connect(dbName)
+if os.path.isfile(dbName1):
+   os.remove(dbName1)
+if os.path.isfile(dbName2):
+   os.remove(dbName2)
+
+db = sqlite3.connect(dbName1)
 cur = db.cursor()
 
 cur.execute('pragma foreign_keys = on')
@@ -33,5 +37,16 @@ cur.execute("insert into P values ('C', 3)")
 
 cur.execute("insert into C values ('C', 333)")
 # cur.execute("insert into C values ('D', 444)")
+
+cur.execute("attach database 'x.db' as db1")
+
+cur.execute('''
+create table db1.CX (
+   id   text   references main.P,
+   val  integer 
+)
+''')
+
+cur.execute("insert into db1.CX values ('C', 333)")
 
 cur.execute('commit')
